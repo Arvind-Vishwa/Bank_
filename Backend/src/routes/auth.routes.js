a0a1374e84d3fc86ee54c23fbcdd7555a864aec1
Arvind-Vishwa <av0216380@gmail.com>
2026-03-27 16:28:26 +0530

    updated auth routes

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,9 +1,12 @@
 const express=require("express");
 const router=express.Router();
-const userRegisterController=require("../controllers/auth.controllers")
+const {userRegisterController, userLoginController}=require("../controllers/auth.controllers")
 
 // api --> /api/auth/register
 router.post('/register',userRegisterController)
+
+// api --> /api/auth/login
+router.post('/login',userLoginController);
 
 
 

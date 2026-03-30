20f1b3ca1f14626a0482f71a7a5b3a9bbaf661a2
Arvind-Vishwa <av0216380@gmail.com>
2026-03-30 19:32:28 +0530

    account model

---
+++ Backend/src/models/account.models.js
@@ -0,0 +1,29 @@
+const  mongoose=require("mongoose");
+
+const accountSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:[true,"Account must be associated with user"],
+        index:true
+    },
+    status:{
+        enum:{
+            values:["ACTIVE","FROZEN","CLOSED"],
+            message:"Status can be ACTIVE,FROZEN and CLOSED"
+        }
+    },
+    currency:{
+        type:String,
+        required:[true,"currency is required for creating an account"],
+        default:"INR"
+    },
+},{
+    timestamps:true
+});
+
+accountSchema.index({user:1,status:1})
+
+const accountModel=mongoose.model("account",accountSchema);
+
+module.exports=accountModel;

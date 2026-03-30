c020778f8244c2d340d5e5a91784e114ccdbf1e2
Arvind-Vishwa <av0216380@gmail.com>
2026-03-30 19:37:51 +0530

    added api of account

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,11 +2,12 @@ const express=require('express');
 const app=express();
 const cookieParser=require('cookie-parser');
 const authRouter=require("./routes/auth.routes");
-
+const accountRouter=require("../src/routes/account.routes")
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth',authRouter);
+app.use('/api/accounts',accountRouter);
 
 
 module.exports=app;

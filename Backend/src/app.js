e970eb4f1117095cbfd4f769cf38a389e3298cfd
Arvind-Vishwa <av0216380@gmail.com>
2026-04-01 16:18:04 +0530

    updated middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,11 +3,12 @@ const app=express();
 const cookieParser=require('cookie-parser');
 const authRouter=require("./routes/auth.routes");
 const accountRouter=require("../src/routes/account.routes")
+const transactionRoutes=require("./routes/transaction.routes")
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth',authRouter);
 app.use('/api/accounts',accountRouter);
-
+app.use("/api/trasactions",transactionRoutes)
 
 module.exports=app;

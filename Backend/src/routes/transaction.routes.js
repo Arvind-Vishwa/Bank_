3b07d7d40bef6ccecf4fc794868a642e100e287a
Arvind-Vishwa <av0216380@gmail.com>
2026-04-02 21:32:00 +0530

    updated transaction route

--- Backend/src/routes/transaction.routes.js
+++ Backend/src/routes/transaction.routes.js
@@ -1,11 +1,14 @@
 const express=require("express");
 
 const router=express.Router();
-const {authMiddleware}=require("../middleware/auth.middleware")
+const {authMiddleware,authSystemUserMiddleware}=require("../middleware/auth.middleware")
+const {createTransaction,createInitialFundsTransaction}=require("../controllers/transaction.controller")
 
 
 // create  a new transaction
-router.post("/",authMiddleware);
+router.post("/",authMiddleware,createTransaction);
 
+// initial fund transaction from system user
+router.post("/system/initial-fund",authSystemUserMiddleware,createInitialFundsTransaction);
 
 module.exports=router;

a8e64b0311d2c1b2e27b7bc1f2d7cdd6fe43b3b1
Arvind-Vishwa <av0216380@gmail.com>
2026-04-02 21:31:21 +0530

    added blackList logout feature

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const router=express.Router();
-const {userRegisterController, userLoginController}=require("../controllers/auth.controllers")
+const {userRegisterController, userLoginController,logoutController}=require("../controllers/auth.controllers")
 
 // api --> /api/auth/register
 router.post('/register',userRegisterController)
@@ -8,7 +8,8 @@ router.post('/register',userRegisterController)
 // api --> /api/auth/login
 router.post('/login',userLoginController);
 
-
+// logout routes --> api/auth/logout
+router.post('/logout',logoutController)
 
 
 module.exports=router;
